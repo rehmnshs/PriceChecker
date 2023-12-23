e999fc5915388851a3e0dd3bf1d814f76218f66b
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2023-12-23 17:23:26 +0530

    ads

--- Be/server.js
+++ Be/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = 5000;
 app.use(express.json());
-app.use(cors({ origin: "https://pricecheckerrr.onrender.com/aw/productpage" }));
+app.use(cors({ origin: "*" }));
 
 // Launch a headless browser.
 
