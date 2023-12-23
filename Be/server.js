ce3102176722b75949769188ec2e3909114922da
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2023-12-23 14:14:00 +0530

    all

--- Be/server.js
+++ Be/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 
-const PORT = 5000;
+const PORT = 6000;
 app.use(express.json());
 app.use(cors()); 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,7 +49,7 @@ export default function Navbar() {
         </div>
         <div className="img1">
           <section>
-           <Carousell />
+
           </section>
           
         </div>
